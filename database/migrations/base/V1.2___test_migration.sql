8244f3177fd87685c1765ef58172045b11ddd988
Ryan McKeever <ryan.mckeever@unosquare.com>
2024-02-20 12:02:36 +0000

    Fix migration

--- database/migrations/base/V1.2___test_migration.sql
+++ database/migrations/base/V1.2___test_migration.sql
@@ -1,7 +1,7 @@
 create table if not exists game_masters_test (
     id serial primary key,
     planning_notes varchar,
-    created timestamp
+    created timestamp,
     player_id int not null,
     constraint fk_player foreign key (player_id)
     	references players(id) on delete cascade
